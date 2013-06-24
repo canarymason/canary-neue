4d0d81e8def9bbaf4bf4758a4dd1c71e6232c8e9
Mason Wendell <mason@canarypromo.com>
2013-06-24 00:16:21 -0400

    move to new style prototypes

---
+++ js/main.js
@@ -0,0 +1,15 @@
+
+$(function() {
+  console.log('Canary Promotion');
+  var jPM = $.jPanelMenu({
+    beforeOpen: function() {
+      $('#wall').remove();
+      $('.jPanelMenu-panel')
+        .append('<div id="wall" data-module-type="Wall"></div>');
+    },
+    beforeClose: function() {
+      $('#wall').remove();
+    }
+  });
+  jPM.on();
+});
