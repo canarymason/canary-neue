2051433803e9dc0bf88d5ed69e486f758ba9c8a6
Mason Wendell <mason@canarypromo.com>
2013-10-08 22:22:35 -0400

    dist

--- Gemfile
+++
@@ -1,6 +0,0 @@
-# Pull gems from RubyGems
-source "https://rubygems.org"
-gem "sass", ">=3.2.0"
-gem "compass", ">=0.12.1"
-gem "style-prototypes", "~>1.3.0"
-gem "singularity-extras", "~>0.0.3"
--- js/main.js
+++ js/main.js
@@ -1,18 +1,1 @@
-$(function () {
-  // $(window).resize(function(){
-  //   if ($('#main-menu-toggle').is(':hidden') && jPM.isOpen()) {
-  //     jPM.close();
-  //   }
-  //   $('.jPanelMenu-panel').css('min-height', $(window).height());
-  // });
-
-  // // flexslider
-  // $('.slideshow').flexslider({
-  //   selector: '.slides > .slide',
-  //   animation: 'slide',
-  //   prevText: 'previous',
-  //   nextText: 'next'
-  // });
-  // $('.flex-direction-nav a').addClass('icon-canary');
-
-});
+$(function(){});
